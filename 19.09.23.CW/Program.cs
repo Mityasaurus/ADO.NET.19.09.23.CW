13e48119a6b7513a627f684607da8904e5fd1a04
Mityasaurus <dmitriybovdun5@gmail.com>
2023-09-19 19:05:38 +0300

    First commit

---
+++ 19.09.23.CW/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _19._09._23.CW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
